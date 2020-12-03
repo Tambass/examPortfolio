00547a5d5c5abc6ffd967d8ba94ba236215c63b0
FabienFaguet <58740890+Tambass@users.noreply.github.com>
2020-12-03 16:58:54 +0100

    Add dependencies required in app.js

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const PORT = 1987;
 const app = express();
-
+const path = require("path");
+const mysql = require("mysql");
+const methodOverride = require("method-override");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const util = require("util");
+const session = require("express-session");
+const fileupload = require("express-fileupload");
 
 
 app.listen(PORT, function(){
