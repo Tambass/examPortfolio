e27995588a97c3670b1c22de59e4bebc2e9882c1
FabienFaguet <58740890+Tambass@users.noreply.github.com>
2020-12-04 15:45:00 +0100

    Get usersHomePage

---
+++ controllers/users_controllers/homePage.js
@@ -0,0 +1,5 @@
+module.exports = {
+    getHomePage: (req, res) => {
+        res.render("users_views/index");
+    }
+}
---
+++ routes/usersRoutes.js
@@ -0,0 +1,10 @@
+module.exports = function (app) {
+    // ****USERS CONTROLLERS****
+
+    // HomePage
+    const {getHomePage} = require("../controllers/users_controllers/homePage");
+
+    // ****USERS ROUTES****
+
+    app.route("/").get(getHomePage);
+}
