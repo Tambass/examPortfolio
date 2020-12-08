f7746b73ddccdc545beb0d6098ccb5ca1c36b93c
FabienFaguet <58740890+Tambass@users.noreply.github.com>
2020-12-08 16:55:56 +0100

    Get admin infos DB to API

--- controllers/admin_controllers/admin_about.js
+++ controllers/admin_controllers/admin_about.js
@@ -1,5 +1,11 @@
 module.exports = {
-    getAboutPage: (req, res) => {
-        res.render("admin_views/admin_about", {title: "About Admin"});
+    getAboutPage: async (req, res) => {
+        const admin = await query("SELECT * FROM Admin")
+
+        try {
+            res.render("admin_views/admin_about", {title: "About Admin", admin: admin[0]});
+        } catch (err) {
+            res.send(err);
+        }
     }
 }
