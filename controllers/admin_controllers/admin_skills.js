731b684a655832caf6fe288d44642b5bf70ef946
FabienFaguet <58740890+Tambass@users.noreply.github.com>
2020-12-09 10:37:51 +0100

    Get database technologies to adminSkillsPage

--- controllers/admin_controllers/admin_skills.js
+++ controllers/admin_controllers/admin_skills.js
@@ -1,6 +1,16 @@
 module.exports = {
-    getSkillsPage: (req, res) => {
-        res.render("admin_views/admin_skills", {title: "Admin Skills"});
+    getSkillsPage: async (req, res) => {
+
+        const skill = await query("SELECT t.technologie_id, t.name AS name, t.image, t.status, tc.name AS category FROM Technologie AS t INNER JOIN Technology_Category AS tc ON tc.technology_category_id = t.technology_category_id;")
+
+        try {
+            res.render("admin_views/admin_skills", {
+                title: "Admin Skills",
+                skill,
+            });
+        } catch (err) {
+            res.send(err);
+        }
     },
     getAddSkillPage: (req, res) => {
         res.render("admin_views/admin_addSkill", {title: "Add Skill"});
