9b4e31e97bb5dfb5bf39f878bd8042d1396e9461
FabienFaguet <58740890+Tambass@users.noreply.github.com>
2020-12-09 10:52:55 +0100

    Get database categories to adminCategoriesPage

--- controllers/admin_controllers/admin_categories.js
+++ controllers/admin_controllers/admin_categories.js
@@ -1,6 +1,19 @@
 module.exports = {
-    getCategoriesPage: (req, res) => {
-        res.render("admin_views/admin_categories", {title: "Admin Categories"});
+    getCategoriesPage: async (req, res) => {
+
+        const projectCategory = await query("SELECT project_category_id AS id, name, status FROM Project_Category");
+
+        const skillCategory = await query("SELECT technology_category_id AS id, name, status FROM Technology_Category");
+        
+        try {
+            res.render("admin_views/admin_categories", {
+                title: "Admin Categories",
+                projectCategory,
+                skillCategory,
+            });
+        } catch (err) {
+            res.send(err);
+        }
     },
     getAddProjectCategoryPage: (req, res) => {
         res.render("admin_views/admin_addProjectCategory", {title: "Add Project Category"});
