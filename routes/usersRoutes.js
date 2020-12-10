bb41a9a368a8f5c56bd79fe62dc0269681cbc9fe
FabienFaguet <58740890+Tambass@users.noreply.github.com>
2020-12-10 16:16:35 +0100

    comment usersRoute.js file

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,10 +1,15 @@
 module.exports = function (app) {
+
+    // *************************
     // ****USERS CONTROLLERS****
+    // *************************
 
     // HomePage
     const {getHomePage} = require("../controllers/users_controllers/homePage");
 
+    // ********************
     // ****USERS ROUTES****
+    // ********************
 
     app.route("/").get(getHomePage);
 }
