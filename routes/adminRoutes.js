e2c725bc30870af1fb45dbcd175415d1ce75b84e
FabienFaguet <58740890+Tambass@users.noreply.github.com>
2020-12-10 11:29:31 +0100

    comment adminRoute file

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,5 +1,8 @@
 module.exports = function (app) {
-    // ****USERS CONTROLLERS****
+
+    // *************************
+    // ****ADMIN CONTROLLERS****
+    // *************************
 
     // HomePage
     const {getAdminPage} = require("../controllers/admin_controllers/admin_homePage");
@@ -46,29 +49,40 @@ module.exports = function (app) {
         changeSkillCategoryStatus,
     } = require("../controllers/admin_controllers/admin_categories");
 
-    // ****USERS ROUTES****
+    // ********************
+    // ****ADMIN ROUTES****
+    // ********************
 
+    // ADMIN HOME PAGE
     app.route("/admin").get(getAdminPage);
 
+    // ADMIN ABOUT PAGE
     app.route("/admin/about").get(getAboutPage);
 
+    // ADMIN PORTFOLIO PAGE
     app.route("/admin/portfolio").get(getPortfolioPage);
     app.route("/admin/portfolio/add").get(getAddProjectPage).post(addProject);
     app.route("/admin/portfolio/edit/:id").get(getEditProjectPage).put(editProject);
     app.route("/admin/portfolio/delete/:id").delete(deleteProject);
     app.route("/admin/portfolio/status/:id").put(changeProjectStatus);
 
+    // ADMIN SKILLS PAGE
     app.route("/admin/skills").get(getSkillsPage);
     app.route("/admin/skills/add").get(getAddSkillPage).post(addSkill);
     app.route("/admin/skills/edit/:id").get(getEditSkillPage).put(editSkill);
     app.route("/admin/skills/delete/:id").delete(deleteSkill);
     app.route("/admin/skills/status/:id").put(changeSkillStatus);
 
+    // ADMIN CATEGORIES PAGE
     app.route("/admin/categories").get(getCategoriesPage);
+
+    // Project_Category part
     app.route("/admin/categories/add_project_category").get(getAddProjectCategoryPage).post(addProjectCategory);
     app.route("/admin/categories/edit_project_category/:id").get(getEditProjectCategoryPage).put(editProjectCategory);
     app.route("/admin/categories/delete_project_category/:id").delete(deleteProjectCategory);
     app.route("/admin/categories/status_project_category/:id").put(changeProjectCategoryStatus);
+
+    // Skill_Category part
     app.route("/admin/categories/add_skill_category").get(getAddSkillCategoryPage).post(addSkillCategory);
     app.route("/admin/categories/edit_skill_category/:id").get(getEditSkillCategoryPage).put(editSkillCategory);
     app.route("/admin/categories/delete_skill_category/:id").delete(deleteSkillCategory);
