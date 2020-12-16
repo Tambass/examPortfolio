f70250ddb25a022143b9b69f395ff024601ec836
FabienFaguet <58740890+Tambass@users.noreply.github.com>
2020-12-16 15:53:05 +0100

    can login

--- controllers/users_controllers/login.js
+++ controllers/users_controllers/login.js
@@ -34,28 +34,36 @@ module.exports = {
         await query(admin, (err, result) => {
 
             // VERIFIE SI L'EMAIL EST CONNU OU CORRECT
-
+            console.log("1 :");
             // Si l'email est inconnu ou incorrect
             if(err || result.length === 0) {
                 res.redirect("/login");
+                console.log("email incorrect !");
             } else {
+                console.log("2");
                 // S'il est connu
                 // Compare le mot de passe hashé
                 bcrypt.compare(password, result[0].password, async (err, success) => {
+                    console.log("3");
                     // S'il y a erreur
                     if(err) {
                         res.redirect("/login");
+                        console.log("Erreur mot de passe !");
+                        console.log("4");
                     }
-
+                    console.log("5");
                     // S'il y a succès
                     if(success) {
+                        console.log("6");
                         const adminHash = "SELECT admin_id, email, password FROM Admin WHERE email = '" +
                         email + "' AND password = '" + result[0].password + "';";
-
+                        
                         await query(
                             adminHash, function(err, result) {
+                                console.log("7");
                                 // Si le résultat est juste
                                 if(result.length) {
+                                    console.log("8");
                                     req.session.loggedin = true,
                                     req.session.cookie.maxAge = 5 * 60 * 1000,
                                     req.session.adminId = result[0].admin_id,
@@ -65,13 +73,19 @@ module.exports = {
                                     res.redirect("/admin")
                                 } else {
                                     res.send(err);
+                                    console.log("MERDE");
                                 }
+                                console.log("9");
                             }
                         );
+                        console.log("10");
 
                     } else {
                         // Si le mot de passe est incorrect
                         res.redirect("/login");
+                        console.log("Mot de pass incorrect !");
+                        console.log("11");
+                        
                     }
                 })
             }
